3abfb24024b011dfde382e25b086a16daf23964a
Shailesh <ShaileshTech@outlook.com>
2021-04-06 19:49:28 -0500

    First commit

---
+++ HeartBeat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServiceWithTopshelf
+{
+    public class HeartBeat
+    {
+        private readonly Timer _timer;
+        public HeartBeat()
+        {
+            _timer = new Timer(2000) { AutoReset = true };
+            _timer.Elapsed += TimeElapsed;
+        }
+
+        private void TimeElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"c:\temp\HeartBeat.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace WindowsServiceWithTopshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<HeartBeat>(s =>
+                {
+                    s.ConstructUsing(heartBeat => new HeartBeat());
+                    s.WhenStarted(heartBeat => heartBeat.Start());
+                    s.WhenStopped(HeartBeat => HeartBeat.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("WindowsTestService");
+                x.SetDisplayName("WindowsTestService");
+                x.SetDescription("This is test service can be stopped");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+
+        }
+    }
+}
